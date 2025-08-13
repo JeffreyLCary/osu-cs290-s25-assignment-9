10822fdc37977dd6f0da68584504a5fcdb05553d
JeffreyLCary <caryjef@oregonstate.edu>
2025-08-12 21:39:07 -0400

    Commented out unimplemented components

--- caryjef-react/src/App.jsx
+++ caryjef-react/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import CreateExercisePage from './pages/CreateExercisePage';
-import EditExercisePage from './pages/EditExercisePage';
+// import CreateExercisePage from './pages/CreateExercisePage';
+// import EditExercisePage from './pages/EditExercisePage';
 
 function App() {
   
@@ -11,8 +11,8 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<HomePage/>}></Route>
-            <Route path="/create-exercise" element={<CreateExercisePage />}></Route>
-            <Route path="/edit-exercise" element={<EditExercisePage />}></Route>
+            {/* <Route path="/create-exercise" element={<CreateExercisePage />}></Route> */}
+            {/* <Route path="/edit-exercise" element={<EditExercisePage />}></Route> */}
           </Routes>
         </Router>
     </div>
